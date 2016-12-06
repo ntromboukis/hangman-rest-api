45d48f84c86b49ee2a5ef85e936b79d7d87b4b36
Nick Tromboukis <ntromboukis@gmail.com>
2016-12-06 15:09:39 -0500

    fix: cron job sends email to users with a game that is not over once a week.

--- main.py
+++ main.py
@@ -16,10 +16,10 @@ class SendReminderEmail(webapp2.RequestHandler):
         """Send a reminder email to each User with an email about games.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
-        users = User.query(User.email != None)
+        users = Game.query().filter(Game.game_over() == False)
         for user in users:
             subject = 'This is a reminder!'
-            body = 'Hello {}, try out Hangman!'.format(user.name)
+            body = 'Hello {}, you have a game still open!'.format(user.name)
             # This will send test emails, the arguments to send_mail are:
             # from, to, subject, body
             mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
